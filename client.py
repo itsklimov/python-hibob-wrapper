ef80c32bc0c08ed44584c6174bd219bf786a39d2
itsklimov <itsklimov@gmail.com>
2024-05-31 10:39:28 -0700

    fixed the content type difference

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import logging
 import requests
 from .exceptions import HibobException
 
-log = logging.getLogger('hibiob.client')
+log = logging.getLogger("hibiob.client")
 log.setLevel(logging.INFO)
 
 
@@ -17,45 +17,43 @@ class Client:
 
     @staticmethod
     def parse_response(response):
-        if response.headers['Content-Type'] != 'application/json':
-            log.debug('Response is not application/json, returning raw response')
+        content_type = response.headers.get("Content-Type", "")
+
+        if content_type != "application/json":
+            log.debug("Response is not application/json, returning raw response")
             return response
 
         try:
             return response.json()
         except ValueError:
-            log.debug('Could not convert response to json, returning raw response')
+            log.debug("Could not convert response to json, returning raw response")
 
         return response
 
-    def make_request(self, method, endpoint, json_body=None, query=None, body=None, files=None):
+    def make_request(
+        self, method, endpoint, json_body=None, query=None, body=None, files=None
+    ):
 
         methods = {
-            'get': requests.get,
-            'post': requests.post,
-            'put': requests.put,
-            'delete': requests.delete
+            "get": requests.get,
+            "post": requests.post,
+            "put": requests.put,
+            "delete": requests.delete,
         }
 
         method = method.lower()
         request = methods.get(method, requests.get)
 
         request_params = {
-            'headers': {
-                'Accept': 'application/json',
-                "Authorization": self.api_token
-            },
-            'json': json_body,
-            'params': query,
-            'data': body,
-            'files': files,
-            'timeout': self.request_timeout
+            "headers": {"Accept": "application/json", "Authorization": self.api_token},
+            "json": json_body,
+            "params": query,
+            "data": body,
+            "files": files,
+            "timeout": self.request_timeout,
         }
 
-        response = request(
-            self.url + endpoint,
-            **request_params
-        )
+        response = request(self.url + endpoint, **request_params)
         parsed_response = self.parse_response(response)
         try:
             response.raise_for_status()
@@ -64,7 +62,7 @@ class Client:
             log.debug(response)
             if isinstance(parsed_response, dict):
                 # response is json and was parsed to dict
-                raise HibobException(parsed_response.get('error'))
+                raise HibobException(parsed_response.get("error"))
 
             # fallback to requests exception
             raise error
@@ -72,13 +70,19 @@ class Client:
         return parsed_response
 
     def get(self, endpoint, query=None):
-        return self.make_request('get', endpoint, query=query)
+        return self.make_request("get", endpoint, query=query)
 
     def post(self, endpoint, json_body=None, query=None, body=None, files=None):
-        return self.make_request('post', endpoint, json_body=json_body, query=query, body=body, files=files)
+        return self.make_request(
+            "post", endpoint, json_body=json_body, query=query, body=body, files=files
+        )
 
     def put(self, endpoint, json_body=None, query=None, body=None, files=None):
-        return self.make_request('post', endpoint, json_body=json_body, query=query, body=body, files=files)
+        return self.make_request(
+            "post", endpoint, json_body=json_body, query=query, body=body, files=files
+        )
 
     def delete(self, endpoint, json_body=None, query=None, body=None, files=None):
-        return self.make_request('post', endpoint, json_body=json_body, query=query, body=body, files=files)
+        return self.make_request(
+            "post", endpoint, json_body=json_body, query=query, body=body, files=files
+        )
