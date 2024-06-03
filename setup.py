413fe8f5e49a88032e22081cc599e39794933c46
itsklimov <itsklimov@gmail.com>
2024-06-03 12:10:03 -0700

    make it installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hibob",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    include_package_data=True,
+    description="Fork of a Python wrapper for the HiBob API",
+    url="https://github.com/itsklimov/python-hibob-wrapper",
+    author="Nikolay Klimov",
+    author_email="itsklimov@gmail.com",
+    license="MIT",
+)
